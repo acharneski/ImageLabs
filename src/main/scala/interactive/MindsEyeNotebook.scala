06e3573d9a3be3004385f1c143adaed3832e7c9c
Andrew Charneski <acharneski@gmail.com>
2017-05-27 11:41:53 -0700

    wip

--- src/main/scala/interactive/MindsEyeNotebook.scala
+++ src/main/scala/interactive/MindsEyeNotebook.scala
@@ -20,13 +20,15 @@
 package interactive
 
 import java.io.{ByteArrayOutputStream, PrintStream}
+import java.util.UUID
 import java.util.concurrent.Semaphore
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.simiacryptus.mindseye.opt.{IterativeTrainer, TrainingMonitor}
 import com.simiacryptus.util.io.{HtmlNotebookOutput, TeeOutputStream}
 import com.simiacryptus.util.text.TableOutput
-import com.simiacryptus.util.{MonitoredObject, StreamNanoHTTPD}
+import com.simiacryptus.util.{ArrayUtil, MonitoredObject, StreamNanoHTTPD}
 import fi.iki.elonen.NanoHTTPD.IHTTPSession
 import smile.plot.{PlotCanvas, ScatterPlot}
 import util.{Java8Util, ScalaNotebookOutput}
@@ -37,14 +39,17 @@ import fi.iki.elonen.NanoHTTPD
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Future
+import ArrayUtil._
 
 abstract class MindsEyeNotebook(server: StreamNanoHTTPD, log: HtmlNotebookOutput with ScalaNotebookOutput) {
+
   val history = new scala.collection.mutable.ArrayBuffer[IterativeTrainer.Step]()
   val logOut = new TeeOutputStream(log.file("log.txt"), true)
   val logPrintStream = new PrintStream(logOut)
   val monitoringRoot = new MonitoredObject()
   val dataTable = new TableOutput()
   def model: NNLayer
+
   val monitor = new TrainingMonitor {
     override def log(msg: String): Unit = {
       println(msg)
@@ -54,7 +59,7 @@ abstract class MindsEyeNotebook(server: StreamNanoHTTPD, log: HtmlNotebookOutput
     override def onStepComplete(currentPoint: IterativeTrainer.Step): Unit = {
       history += currentPoint
       val iteration = currentPoint.iteration
-      if(shouldReplotMetrics(iteration)) regenerateLog()
+      if(shouldReplotMetrics(iteration)) regenerateReports()
       def flatten(prefix:String,data:Map[String,AnyRef]) : Map[String,AnyRef] = {
         data.flatMap({
           case (key, value) ⇒ value match {
@@ -78,7 +83,6 @@ abstract class MindsEyeNotebook(server: StreamNanoHTTPD, log: HtmlNotebookOutput
     case _ if 100 > iteration ⇒ false
     case _ if 0 == iteration % 100 ⇒ true
     case _ ⇒ false
-
   }
 
   def defineMonitorReports(log: HtmlNotebookOutput with ScalaNotebookOutput = log): Unit = {
@@ -129,12 +133,49 @@ abstract class MindsEyeNotebook(server: StreamNanoHTTPD, log: HtmlNotebookOutput
     }
   }
 
-  def regenerateLog() = {
-    val file = log.file("../metricsHistory.html")
-    val report = new HtmlNotebookOutput(log.workingDir, file) with ScalaNotebookOutput
-    generateLog(report).andThen({case _⇒report.close()})
+  def regenerateReports() = {
+    Option(log.file("../metricsHistory.html")).foreach(file⇒{
+      val report = new HtmlNotebookOutput(log.workingDir, file) with ScalaNotebookOutput
+      generateMetricsHistoryReport(report).andThen({case _⇒report.close()})
+    })
+    Option(log.file("../mobility.html")).foreach(file⇒{
+      val report = new HtmlNotebookOutput(log.workingDir, file) with ScalaNotebookOutput
+      generateMobilityReport(report).andThen({case _⇒report.close()})
+    })
   }
-  def generateLog(log: ScalaNotebookOutput = log): Future[Unit] = {
+
+  def generateMobilityReport(log: ScalaNotebookOutput = log): Future[Unit] = Future {
+    if (!history.isEmpty) {
+      val layers: Array[NNLayer] = history.flatMap(_.point.weights.map.asScala.keySet).distinct.toArray
+      log.out("<table>")
+      layers.foreach(layer ⇒ {
+        try {
+          val transcript: List[Array[Double]] = history.map(_.point.weights.map.get(layer).delta).toList
+          log.out("<tr><td>")
+          log.p(s"Layer ${layer.getClass}<br/>id ${layer.id}<br/>")
+          List(1, 5, 20).foreach(lag ⇒ {
+            log.out("</td><td>")
+            val xy = (lag until transcript.size).map(i ⇒ {
+              i → Math.log(Math.sqrt(magnitude(subtract(transcript(i), transcript(i - lag)))))
+            }).filter(d ⇒ java.lang.Double.isFinite(d._2))
+            if (xy.size > 1) log.eval {
+              val plot: PlotCanvas = ScatterPlot.plot(xy.map(xy ⇒ Array(xy._1.toDouble, xy._2)): _*)
+              plot.setTitle(s"${layer.getClass}/${layer.id}")
+              plot.setAxisLabels("Epoch", s"log(dist(n,n-$lag))")
+              plot.setSize(600, 400)
+              plot
+            } else log.out("No Data")
+          })
+          log.out("</td></tr>")
+        } catch {
+          case e: Throwable ⇒
+        }
+      })
+      log.out("</table>")
+    }
+  }
+
+  def generateMetricsHistoryReport(log: ScalaNotebookOutput = log): Future[Unit] = {
     if(!history.isEmpty) {
       val dataAsScala: Array[Map[String, AnyRef]] = dataTable.rows.asScala.map(_.asScala.toMap).toArray
       val keys: Array[String] = dataTable.schema.asScala.keySet.toArray
@@ -149,29 +190,31 @@ abstract class MindsEyeNotebook(server: StreamNanoHTTPD, log: HtmlNotebookOutput
           .foreach(key⇒{
             log.out("<tr><td>")
             try {
-              log.eval {
-                val plot: PlotCanvas = ScatterPlot.plot(dataAsScala.map(row ⇒ Array[Double](
-                  row("epoch").toString.toDouble, row(key).toString.toDouble
-                )): _*)
+              val data = dataAsScala.map(row ⇒ Array[Double](
+                row("epoch").toString.toDouble, row(key).toString.toDouble
+              )).filter(d ⇒ d.forall(java.lang.Double.isFinite))
+              if(data.size > 1) log.eval {
+                val plot: PlotCanvas = ScatterPlot.plot(data: _*)
                 //plot.setTitle(s"$key vs Epoch")
                 plot.setAxisLabels("Epoch", key)
                 plot.setSize(600, 400)
                 plot
-              }
+              } else log.out("No Data")
             } catch {
               case e : Throwable ⇒
             }
             log.out("</td><td>")
             try {
-              log.eval {
-                val plot: PlotCanvas = ScatterPlot.plot(dataAsScala.map(row ⇒ Array[Double](
-                  Math.log(row("value").toString.toDouble), row(key).toString.toDouble
-                )): _*)
+              val data = dataAsScala.map(row ⇒ Array[Double](
+                Math.log(row("value").toString.toDouble), row(key).toString.toDouble
+              )).filter(d ⇒ d.forall(java.lang.Double.isFinite))
+              if(data.size > 1) log.eval {
+                val plot: PlotCanvas = ScatterPlot.plot(data: _*)
                 //plot.setTitle(s"$key vs Epoch")
                 plot.setAxisLabels("log(value)", key)
                 plot.setSize(600, 400)
                 plot
-              }
+              } else log.out("No Data")
             } catch {
               case e : Throwable ⇒
             }
